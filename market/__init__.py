ef04c9fa77e5b9a3204a21bdf27a274846e13539
Aniket Chavan <aniketchavan214@gmail.com>
2023-09-19 00:00:19 +0530

    added login and register functionality

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '153f5c068ec6cac85d69cce1'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
 app.app_context().push()
 
 from market import routes
